5943ea0c2c340e204741d11c15a89f3558baa734
Kevin <kevinzang@berkeley.edu>
2014-02-15 21:35:13 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitplan::Application.config.secret_key_base = '92682620270da435885a7b070c905060d59628d65cbe0daea8cbd847c29ee15f432025038b5d42f5b60c73f28dbc23648879b7b64e50cf7aa6c3a739b62f4521'
